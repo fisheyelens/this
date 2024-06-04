e70b26ce126f4326910ce1b8e1aeaa844baae609
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-04 10:35:55 +0900

    && 사용.

--- work-1.js
+++ work-1.js
@@ -1,9 +1,11 @@
 
 function workOne(a,b){
-  if (typeof(a) === 'number'){
-    if(typeof(b) === 'number'){
+  if (typeof(a) === 'number' && typeof(b) === 'number' ){
+  
       let local = a+b;
       return local;
-    }
+    
+  } else {
+    console.error("매개변수 잘못 작성됨");
   }
-}
+};
