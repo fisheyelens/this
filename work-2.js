3e3499c4dd2840eae29776e183215269e63a6225
Jee Won Baek <mermaidstar103@gmail.com>
2024-06-04 10:51:25 +0900

    setter 만들어서 에러 처리(throw 사용)

--- work-2.js
+++ work-2.js
@@ -4,7 +4,23 @@ constructor(a,b){
   this.a = a;
   this.b = b;
   }
+// * 규격 함수 만들기
+// * value는 a를 지칭한다. 첫번째 매개변수가 있어야 한다. 
+set a(value) {
+  if(typeof(value) === 'number') {
+    return this._a = value;
+    } else {
+      throw new Error("a는 숫자만 입력이 가능합니다");
+      // * new error 또한 생성자 함수 ( 불러오는것)
+      // ! 원하지 않는 것은 에러처리 해버리겠다. 에러는 반가운 일이다. 
+      // ! 에러 없이 실행되는 것이 사고다.
+      // ! 에러를 일부러 발생시키고 있다. 틀린 작성법이 아니다.
+    }
+  }
+  // * 관례적으로 언더바 ( 색터를 통과했다는  뜻)
 }
 
-let test = new WorkTwo(1,2);
+let test = new WorkTwo("짜장",2);
+// * number 가 아니기에, 입력되지 않는다. 
+
 console.log(test);
